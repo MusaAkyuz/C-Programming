eb66f27fc17ed2b1162a62b3bc00a9015e96f218
MusaAkyuz <musakyuz0@gmail.com>
2023-01-04 16:33:33 +0300

    update

---
+++ Arduino/Receiver_NRF24/Receiver_NRF24.ino
@@ -0,0 +1,15 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[6] = "robotistan100";
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+ const char text[] = "Hello World"; // maksimium 32 karakter olabilir
+ radio.write(&text, sizeof(text));
+}
---
+++ Arduino/Transmitter_NRF24/Transmitter_NRF24.ino
@@ -0,0 +1,19 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(9, 10); // CE, CSN
+const byte address[10] = "robotistan100";
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+void loop() {
+  if (radio.available()) {
+    char text[32] = "";//print only 32 characters
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
